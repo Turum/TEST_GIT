7d77ff81edd4b154e5ec03858bf7ca1c1074df90
Turum <cicl@yandex.ru>
2018-03-26 22:40:19 +0300

    test

---
+++ CSharpProject/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi CSharp!!! You are pretty.");
+            Console.ReadKey();
+        }
+    }
+}
